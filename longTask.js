280fc9ba1d69c645eb897da4c74d53b4cdde8998
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-02-22 21:05:06 +0330

    make changes

---
+++ longTask.js
@@ -0,0 +1,16 @@
+function longTask() {
+    let sum = 0;
+    for (let index = 0; index < 1e9; index++) {
+        sum += index
+    }
+    return sum
+}
+process.on("message", message => {
+    if(message === "start-process"){
+        const sum = longTask()
+        process.send(sum)
+    }
+})
+process.on("warning", message => {
+    console.log(message);
+})
