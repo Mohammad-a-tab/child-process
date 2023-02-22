0f0cdeca7d18602fc105562daab708d62d5a16ee
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-02-22 21:08:36 +0330

    make changes

--- execFile.js
+++ execFile.js
@@ -1,12 +1,13 @@
-const {execFile} = require("child_process")
+
+const {execFile} = require("child_process");
 const {exec} = require("child_process");
-exec("chmod +x exec.js", (err, stdout, stderr) => {
-    if(err) return console.log("Error :", err.message);
-    if(stderr) return console.log("StdError :", stderr);
-    console.log("stdout : ", stdout);
+exec("chmod +x index.py", (err, stdout, stderr) => {
+    if(err) return console.log("Error: ", err.message)
+    if(stderr) return console.log("stdError: ", stderr);
+    console.log("stdout: ", stdout);
 })
-execFile("./exec.js", (err, stdout, stderr) => {
-    if(err) return console.log("Error :", err.message);
-    if(stderr) return console.log("StdError :", stderr);
-    console.log("stdout : ", stdout);
+execFile("./index.py", (err, stdout, stderr) => {
+    if(err) return console.log("Error: ", err.message)
+    if(stderr) return console.log("stdError: ", stderr);
+    console.log("stdout: ", stdout);
 })
